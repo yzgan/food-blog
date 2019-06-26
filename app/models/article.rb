573170760bb349ddf2ba4c71307cd35f072c4717
Gan Yi Zhong <ganyizhong@gmail.com>
2019-06-26 12:00:42 +0800

    fix textarea styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   has_rich_text :content
   has_many_attached :images
+
+  validates :name, :content, presence: true
+  validates :name, uniqueness: true
 end
--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -370,7 +370,7 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
     b.use :label, class: 'form-control-label'
-    b.use :input, class: 'custom-form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
     b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
     b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
   end
